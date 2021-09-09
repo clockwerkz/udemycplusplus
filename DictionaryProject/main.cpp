9a935f17d7f9ce7b66e7d9b2da051620acd2bd11
Carlos <clockwerkz@gmail.com>
2021-09-09 14:29:04 -0700

    working on dictionary loop

--- DictionaryProject/main.cpp
+++ DictionaryProject/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "Dictionary.h"
 
+void toLower(std::string& word);
+const std::string ADD = "add";
+
 int main() {
 	Dictionary dict = Dictionary();
 	dict.AddDefinition("Addition", "Adding values together for a larger value");
@@ -8,5 +11,27 @@ int main() {
 	std::cout << "Looking up Google should return NOT FOUND: " << dict.GetDefinition("Google") << std::endl;
 	std::cout << "Printing dict:\n-----------\n";
 	dict.PrintAll();
+	std::string choice, word, definition;
+	std::cout << "Welcome to the Dictionary Manager!\n\n";
+	while (choice == "exit") {
+		std::cout << "Would you like to: add, get, print, or exit? ";
+		std::cin >> choice;
+		toLower(choice);
+		switch (choice) {
+		case ADD:
+			std::cout
+		default: 
+			std::cout << "I'm sorry, I did not understand that command\n";
+		}
+	}
+	std::string test = "TEST";
+	toLower(test);
+	std::cout << test << std::endl;
 	return 0;
+}
+
+void toLower(std::string& word) {
+	for (auto& c : word) {
+		c = tolower(c);
+	};
 }
