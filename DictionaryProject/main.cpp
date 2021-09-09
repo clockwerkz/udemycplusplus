0066216cdaa1180c47cdc74c80f0fb7c8ba6c958
Carlos <clockwerkz@gmail.com>
2021-09-09 16:00:24 -0700

    adding loop

--- DictionaryProject/main.cpp
+++ DictionaryProject/main.cpp
@@ -1,32 +1,57 @@
 #include <iostream>
+#include <map>
 #include "Dictionary.h"
+
+enum option {
+	add,
+	get,
+	print,
+	exit
+};
+
+const std::map<std::string, option> lookup{
+	{"add", add},
+	{"get", get},
+	{"print", print},
+	{"exit", exit}
+};
 
 void toLower(std::string& word);
 const std::string ADD = "add";
 
 int main() {
 	Dictionary dict = Dictionary();
-	dict.AddDefinition("Addition", "Adding values together for a larger value");
-	std::cout << "Definition for Addition: " << dict.GetDefinition("Addition") << std::endl;
-	std::cout << "Looking up Google should return NOT FOUND: " << dict.GetDefinition("Google") << std::endl;
-	std::cout << "Printing dict:\n-----------\n";
-	dict.PrintAll();
+	bool isComplete = false;
 	std::string choice, word, definition;
 	std::cout << "Welcome to the Dictionary Manager!\n\n";
-	while (choice == "exit") {
+	while (!isComplete) {
 		std::cout << "Would you like to: add, get, print, or exit? ";
 		std::cin >> choice;
 		toLower(choice);
-		switch (choice) {
-		case ADD:
-			std::cout
+		switch (lookup["edit"]) {
+		case add:
+			std::cout << "Please enter the new term to add into the dictionary:\n";
+			std::cin >> word;
+			std::cout << "Enter the definition:\n";
+			std::cin.ignore();
+			getline(std::cin, definition);
+			dict.AddDefinition(word, definition);
+			break;
+		case get:
+			std::cout << "Please enter word to look up in the dictionary:\n";
+			std::cin >> word;
+			definition = dict.GetDefinition(word);
+			std::cout << word << " : " << definition << std::endl;
+		case print:
+			dict.PrintAll();
+			break;
+		case exit:
+			isComplete = true;
+			break;
 		default: 
 			std::cout << "I'm sorry, I did not understand that command\n";
 		}
 	}
-	std::string test = "TEST";
-	toLower(test);
-	std::cout << test << std::endl;
 	return 0;
 }
 
