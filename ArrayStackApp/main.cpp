a0fdcc75d75359260166e57e8545ad050ec400d4
Carlos <clockwerkz@gmail.com>
2021-09-12 13:52:30 -0700

    adds stackPrint challenge solution

--- ArrayStackApp/main.cpp
+++ ArrayStackApp/main.cpp
@@ -1,18 +1,30 @@
 #include <iostream>
 #include "ArrayStack.h"
+
+void printStack(ArrayStack* &stack);
 int main() {
 	ArrayStack* stack = new ArrayStack();
 	stack->push(80);
 	stack->push(304);
-	std::cout << "stack peek: " << stack->peek() << std::endl;
 	for (int i = 0; i < 5; i++) {
 		stack->push(i);
 	}
-	std::cout << "stack peek: " << stack->peek() << std::endl;
-	while (!stack->isEmpty()) {
-		int el = stack->pop();
-		std::cout << "element removed: " << el << std::endl;
-	}
+	std::cout << "current top of stack: " << stack->peek() << std::endl;
+	printStack(stack);
 	delete stack;
 	return 0;
+}
+
+void printStack(ArrayStack* &stack) {
+	ArrayStack* temp = new ArrayStack();
+	while (!stack->isEmpty()) {
+		temp->push(stack->pop());
+	}
+	int count = 1;
+	while (!temp->isEmpty())
+	{
+		stack->push(temp->pop());
+		std::cout << "element " << count++ << ": " << stack->peek() << std::endl;
+	}
+	delete temp;
 }
