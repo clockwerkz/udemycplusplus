2d5f84317462ef67493dd22d1b0845accc48b8e5
clockwerkz <clockwerkz@gmail.com>
2021-09-13 09:02:09 -0700

    update to tests

--- LinkedQueueProject/main.cpp
+++ LinkedQueueProject/main.cpp
@@ -3,8 +3,10 @@
 
 int main() {
 	LinkQueue* queue = new LinkQueue();
-	for (int i = 1; i < 31; i++) {
+	for (int i = 1; i < 6; i++) {
+		std::cout << "Empty? " << std::boolalpha << queue->isEmpty() << " - queuing: " << i * 100 << std::endl;
 		queue->enqueue(i*100);
+		std::cout << "Peeking: " << queue->peekFront() << std::endl;
 	}
 	std::cout << "finished" << std::endl;
 	while (!queue->isEmpty()) {
@@ -13,7 +15,6 @@ int main() {
 	for (int i = 5; i < 37; i++) {
 		queue->enqueue(i);
 	}
-	std::cout << "Peeking: " << queue->peekFront() << std::endl;
 	while (!queue->isEmpty()) {
 		std::cout << "dequeuing: " << queue->dequeue() << std::endl;
 	}
